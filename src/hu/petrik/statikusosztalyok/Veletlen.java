60b6d1d479ece89e5d8a96f666d6f5d00a1d57ea
FZsiga02 <fzsigmond02@gmail.com>
2022-10-06 09:05:42 +0200

    Véletlen név generálás

--- src/hu/petrik/statikusosztalyok/Veletlen.java
+++ src/hu/petrik/statikusosztalyok/Veletlen.java
@@ -12,8 +12,8 @@ public final class Veletlen {
 
     private static final Random rnd = new Random();
     private static final List<String> vezNevek = feltolt("files/veznev.txt");
-    private static final List<String> ferfiKerNevek = feltolt("files/veznev.txt");;
-    private static final List<String> noiKerNevek = feltolt("files/veznev.txt");;
+    private static final List<String> ferfiKerNevek = feltolt("files/ferfikernev.txt");;
+    private static final List<String> noiKerNevek = feltolt("files/noikernev.txt");;
 
     private static List<String> feltolt(String fajlnev) {
         List<String> lista = new ArrayList<>();
@@ -37,5 +37,41 @@ public final class Veletlen {
 
     public static char velKarakter(char min, char max){
         return (char) velEgesz(min, max);
+    }
+
+    public static String velVezetekNev(){
+        return  vezNevek.get(rnd.nextInt(vezNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar keresztnév generálása
+     * @param nem A generált keresztnév neme. Férfi esetén true, Nő esetén false.
+     * @return A generált keresztnév
+     */
+    public static String velKeresztNev(boolean nem){
+        String keresztNev;
+        if (nem){
+            keresztNev = velFerfiKeresztNev();
+        } else {
+            keresztNev = velNoiKeresztNev();
+        }
+        return keresztNev;
+    }
+
+    private static  String velFerfiKeresztNev(){
+        return ferfiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    private static  String velNoiKeresztNev(){
+        return noiKerNevek.get(rnd.nextInt(noiKerNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar név generálása
+     * @param nem A generált név neme. Férfi esetén true, Nő esetén false.
+     * @return A generált név
+     */
+    public static  String velTeljesNev(boolean nem){
+        return velVezetekNev() + " " + velKeresztNev(nem);
     }
 }
